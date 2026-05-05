2de61adf6c970d7a9f2a3d28dcc8bdbf7fcac31a
m20ja1 <264754617+m20ja1@users.noreply.github.com>
2026-05-05 02:13:37 +0000

    [Add] コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  get "posts/index"
+  get "posts/new"
+  get "posts/create"
+  get "posts/show"
+  get "posts/edit"
+  get "posts/update"
+  get "posts/destroy"
+  get "users/index"
+  get "users/new"
+  get "users/create"
+  get "users/show"
+  get "users/edit"
+  get "users/update"
+  get "users/destroy"
+  get "homes/top"
   # 認証（自動生成）
   resource :session
   resources :passwords, param: :token
