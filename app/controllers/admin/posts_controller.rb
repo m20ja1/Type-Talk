a2025c00cf90b2634538b57989a955ce00999d6d
m20ja1 <264754617+m20ja1@users.noreply.github.com>
2026-05-11 13:53:07 +0000

    [Add]管理者機能

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,12 @@
+class Admin::PostsController < Admin::ApplicationController
+  
+def index
+    @posts = Post.includes(:user).order(created_at: :desc)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to admin_posts_path, notice: "投稿を削除しました"
+  end
+end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,8 @@
 class Admin < ApplicationRecord
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  # バリデーション
+  validates :email_address, presence: true, uniqueness: true
 end
