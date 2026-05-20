070a143fb607696e81804c6ecb7751168fe14f02
m20ja1 <264754617+m20ja1@users.noreply.github.com>
2026-05-20 16:52:07 +0000

    [Fix]通知機能ドロップダウン修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -6,15 +6,7 @@ class Favorite < ApplicationRecord
   validates :user_id, uniqueness: { scope: :post_id }
 
   after_create do
-    create_notification(user_id: post.user_id)
+    notifications.create(user: post.user)
   end
 
-  private
-
-  def create_notification(user_id:)
-    Notification.create!(
-      user_id: user_id,
-      notifiable: self
-    )
-  end
 end
