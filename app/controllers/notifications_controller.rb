d979b7e1ae8fb2b4b2310d75780c0fa6c734e8e2
m20ja1 <264754617+m20ja1@users.noreply.github.com>
2026-06-29 01:25:54 +0000

    [Fix]通知コントローラーのタイプミス修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,8 +7,8 @@ class NotificationsController < ApplicationController
   end
 
   def update
-    notfication = current_user.notifications.find(params[:id])
-    notfication.update(read: true)
+    notification = current_user.notifications.find(params[:id])
+    notification.update(read: true)
     case notification.notifiable_type
     when "Post"
       redirect_to post_path(notification.notifiable)
